653267fa2bd3b0d5fc261472b8cd77b3b37603b6
pevaldes <pedro.valdes397@gmail.com>
2023-11-17 19:56:11 +0100

    git commit

--- NMSM/rejection_method.c
+++ NMSM/rejection_method.c
@@ -9,13 +9,13 @@ void random_s(){
 	srand((unsigned) time(&t));
 }
 
-double rejection(double p, int N){
+double rejection(double p, int c, int N){
     double pi = 4*atan(1.);
     int i = 0;
     double rho, g;
     double ratio;
     double A;
-    double xi, c;
+    double xi;
     double x,y;
     double distg[N], distfx[N], distfy[N];
 
@@ -23,7 +23,6 @@ double rejection(double p, int N){
     
     for(i = 0; i < N; i++){
         xi = (rand() % 1000000)/1000000.;
-        c = (rand() % 1000000)/1000000.;
         x = sqrt( -log( (-2*p/A) * exp(-pow(p,2)) * (xi-A*p-A/(2*p)) ) ); // Random number according to g
         rho = sqrt(2/pi)*exp(-pow(x,2)); // f
         if(x>p){
@@ -32,18 +31,18 @@ double rejection(double p, int N){
         else{
             g = A;
         }
-        ratio = rho/g;
+        ratio = rho/(c*g);
         if(xi>ratio){
             distg[i] = x; // Accept values according to the rejection method
         }
-        y = xi*g;
+        y = c*xi*g;
         if(y<rho){ // Accept the points y<rho to sample values according to rho
             distfx[i] = x;
             distfy[i] = y;
         }
     }
 
-    FILE* file = fopen("rejection_sampling_p05.txt", "w");
+    FILE* file = fopen("rejection_sampling_p03.txt", "w");
     fprintf(file,"x;y\n");
     int loop;
     for(loop = 0; loop < N; loop++){
@@ -55,5 +54,5 @@ double rejection(double p, int N){
 double main(int argn, char **argc){
 	int N = 100000;
 	
-	rejection(0.5,N);
+	rejection(0.3,2,N);
 }
